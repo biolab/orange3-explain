0b307e67e3c35e21dd4d36e88ddcea9ad181dc01
Primož Godec <p.godec9@gmail.com>
2021-01-12 13:15:24 +0100

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 NAME = "Orange3-Explain"
 
-VERSION = "0.1.1"
+VERSION = "0.2.0"
 
 AUTHOR = "Bioinformatics Laboratory, FRI UL"
 AUTHOR_EMAIL = "contact@orange.biolab.si"
