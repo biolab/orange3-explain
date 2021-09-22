57ec7f2529e04bc89f8d17d91f7dc43d451e5daa
Primoz Godec <p.godec9@gmail.com>
2021-09-22 12:56:24 +0200

    Documentation: change builder to html

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ def include_documentation(local_dir, install_dir):
 
 
 if __name__ == "__main__":
-    include_documentation("doc/_build/htmlhelp", "help/orange3-explain")
+    include_documentation("doc/_build/html", "help/orange3-explain")
     setup(
         author=AUTHOR,
         author_email=AUTHOR_EMAIL,
