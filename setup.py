2a131df130fb486b953ab3ccd69a4431f9c0ae2b
Vesna Tanko <tankovesna@hotmail.com>
2022-04-20 08:08:17 +0200

    Release 0.5.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 NAME = "Orange3-Explain"
 
-VERSION = "0.5.2"
+VERSION = "0.5.3"
 
 AUTHOR = "Bioinformatics Laboratory, FRI UL"
 AUTHOR_EMAIL = "contact@orange.biolab.si"
