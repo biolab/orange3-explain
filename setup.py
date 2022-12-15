4debd48cc952e8fcb804fc35c8ce5035b194cd53
PrimozGodec <p.godec9@gmail.com>
2022-12-15 12:27:51 +0100

    Release version 0.6.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 NAME = "Orange3-Explain"
 
-VERSION = "0.6.1"
+VERSION = "0.6.2"
 
 AUTHOR = "Bioinformatics Laboratory, FRI UL"
 AUTHOR_EMAIL = "contact@orange.biolab.si"
